bde157a6b5f83e32b5c2e8e7e983f8a5402955cb
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-02-26 10:01:10 -0500

    does SOME stuff

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { Command } from "commander";
+import { execSync } from "child_process";
+import { copy, mkdir, writeFile } from "fs-extra";
+import path from "path";
 
 const program = new Command()
     .version("1.0.0")
@@ -17,14 +20,62 @@ const program = new Command()
         "create-aeroclient is an easy-to-use CLI for creating a Discord bot using AeroWare's AeroClient.\n" +
             "See its features at https://aero-ware.github.io/aeroclient\n"
     )
-    .action(async (name, options, _command) => {
-        console.log(
-            `Creating an AeroClient project named ${name} in ${
-                options.typescript ? "typescript" : "javascript"
-            } ${options.example ? `from ${options.example}` : ""}...`
-        );
+    .action(
+        async (
+            name: string,
+            options: {
+                typescript: boolean;
+                example: string;
+                "example-path": string;
+            },
+            _command
+        ) => {
+            console.log(
+                `Creating an AeroClient project named ${name} in ${
+                    options.typescript ? "typescript" : "javascript"
+                } ${options.example ? `from ${options.example}` : ""}...`
+            );
 
-        // todo: do the actual things
-    });
+            const projectRoot = path.join(process.cwd(), name);
+
+            // todo: do the actual things
+            try {
+                await mkdir(projectRoot);
+                console.log(
+                    execSync("npm init -y", {
+                        cwd: projectRoot,
+                    }).toString()
+                );
+                await mkdir(path.join(projectRoot, "src"));
+                if (options.typescript) {
+                    await copy(
+                        "../templates/ts",
+                        path.join(projectRoot, "src")
+                    );
+                    console.log(
+                        execSync("npm i typescript tsc", {
+                            cwd: projectRoot,
+                        }).toString()
+                    );
+                    console.log(
+                        execSync("npm i --save-dev @types/node", {
+                            cwd: projectRoot,
+                        }).toString()
+                    );
+                } else {
+                    await copy(
+                        "../templates/js",
+                        path.join(projectRoot, "src")
+                    );
+                }
+                await copy("../templates/.env", path.join(projectRoot, ".env"));
+                console.log(
+                    execSync("npm i @aeroware/aeroclient dotenv", {
+                        cwd: projectRoot,
+                    }).toString()
+                );
+            } catch (e) {}
+        }
+    );
 
 program.parseAsync(process.argv);
